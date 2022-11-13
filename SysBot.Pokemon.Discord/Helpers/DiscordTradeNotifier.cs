c29d5022478855f90d6205c271f9bc96801e6d20
Santacrab2 <skaradiodj@gmail.com>
2022-11-13 09:59:57 -0500

    modifying slash commands to handle generic types now that I know how to do that. Thanks OG Sysbot crew for having that example in syscord.cs!

--- SysBot.Pokemon.Discord/Helpers/DiscordTradeNotifier.cs
+++ SysBot.Pokemon.Discord/Helpers/DiscordTradeNotifier.cs
@@ -31,7 +31,7 @@ namespace SysBot.Pokemon.Discord
 
         public void TradeInitialize(PokeRoutineExecutor<T> routine, PokeTradeDetail<T> info)
         {
-            if (Data is not PB7)
+            if (Data is not T)
             {
                 var receive = Data.Species == 0 ? string.Empty : $" ({Data.Nickname})";
                 Trader.SendMessageAsync($"Initializing trade{receive}. Please be ready. Your code is **{Code:0000 0000}**.").ConfigureAwait(false);
@@ -47,7 +47,7 @@ namespace SysBot.Pokemon.Discord
 
         public void TradeSearching(PokeRoutineExecutor<T> routine, PokeTradeDetail<T> info)
         {
-            if (Data is not PB7)
+            if (Data is not T)
             {
                 var name = Info.TrainerName;
                 var trainer = string.IsNullOrEmpty(name) ? string.Empty : $", {name}";
