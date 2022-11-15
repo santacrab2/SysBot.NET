91f37c4fb9cf659e95a31507f262f5716067bc27
Santacrab2 <skaradiodj@gmail.com>
2022-11-15 12:22:10 -0500

    secret sauce

---
+++ SysBot.Pokemon/SV/SVTestSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using PKHeX.Core;
+using System.ComponentModel;
+using System.Threading;
+using SysBot.Base;
+
+namespace SysBot.Pokemon
+{
+    public class SVTestSettings : IBotStateSettings
+    {
+        private const string SVTestSetting = nameof(SVTestSettings);
+        public override string ToString() => "Test Bot Settings";
+        [Category(SVTestSetting)]
+        public string filename { get; set; }
+
+        public bool ScreenOff { get; set; } = false;
+    }
+}
