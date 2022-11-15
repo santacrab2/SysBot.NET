43a47209bee311a0afa8765c05b18753dca57d5a
Santacrab2 <skaradiodj@gmail.com>
2022-11-15 12:22:10 -0500

    secret sauce

---
+++ SysBot.Pokemon/SV/PokeRoutineExecutor9SCVI.cs
@@ -0,0 +1,112 @@
+﻿using PKHeX.Core;
+using SysBot.Base;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static SysBot.Base.SwitchButton;
+using static SysBot.Pokemon.PokeDataOffsetsSCVI;
+
+namespace SysBot.Pokemon
+{
+    public abstract class PokeRoutineExecutor9SCVI : PokeRoutineExecutor<PK8>
+    {
+        public static uint GetBoxSlotOffset(ulong BoxStartOffset,int box, int slot) => (uint)BoxStartOffset + (uint)(EncryptedSize * ((30 * box) + slot));
+
+        protected PokeDataOffsetsSCVI Offsets { get; } = new();
+        protected PokeRoutineExecutor9SCVI(PokeBotState cfg) : base(cfg) { }
+
+        public new LanguageID GameLang { get; private set; }
+        public new GameVersion Version { get; private set; }
+
+        public override void SoftStop() => Config.Pause();
+
+        public new async Task Click(SwitchButton b, int delayMin, int delayMax, CancellationToken token) =>
+            await Click(b, Util.Rand.Next(delayMin, delayMax), token).ConfigureAwait(false);
+
+        public new async Task SetStick(SwitchStick stick, short x, short y, int delayMin, int delayMax, CancellationToken token) =>
+            await SetStick(stick, x, y, Util.Rand.Next(delayMin, delayMax), token).ConfigureAwait(false);
+
+        public override async Task<PK8> ReadPokemon(ulong offset, int size, CancellationToken token)
+        {
+            var data = await Connection.ReadBytesAsync((uint)offset, size, token).ConfigureAwait(false);
+            return new PK8(data);
+        }
+        public async Task SetBoxPokemon(PK8 pkm, int box, int slot, CancellationToken token, ITrainerInfo? sav = null)
+        {
+            if (sav != null)
+            {
+                // Update PKM to the current save's handler data
+                DateTime Date = DateTime.Now;
+                pkm.Trade(sav, Date.Day, Date.Month, Date.Year);
+                pkm.RefreshChecksum();
+            }
+            var (valid, offset) = await ValidatePointerAll(Offsets.BoxStartPokemonPointer, token);
+            var ofs = GetBoxSlotOffset(offset,box, slot);
+            pkm.ResetPartyStats();
+            await Connection.WriteBytesAsync(pkm.EncryptedPartyData, ofs, token).ConfigureAwait(false);
+        }
+        public override async Task<PK8> ReadPokemonPointer(IEnumerable<long> jumps, int size, CancellationToken token)
+        {
+            var (valid, offset) = await ValidatePointerAll(jumps, token).ConfigureAwait(false);
+            if (!valid)
+                return new PK8();
+            return await ReadPokemon(offset, token).ConfigureAwait(false);
+        }
+
+        public async Task<SAV8SWSH> SCVIIdentifyTrainer(CancellationToken token)
+        {
+            Log("Grabbing trainer data of host console...");
+            SAV8SWSH sav = await SCVIGetFakeTrainerSAV(token).ConfigureAwait(false);
+            GameLang = (LanguageID)sav.Language;
+            Version = sav.Version;
+           // InGameName = sav.OT;
+            Connection.Label = $"{InGameName}-{sav.DisplayTID:000000}";
+            Log($"{Connection.Name} identified as {Connection.Label}, using {GameLang}.");
+
+            return sav;
+        }
+
+        public static void DumpPokemon(string folder, string subfolder, PKM pk)
+        {
+            if (!Directory.Exists(folder))
+                return;
+            var dir = Path.Combine(folder, subfolder);
+            Directory.CreateDirectory(dir);
+            var fn = Path.Combine(dir, Util.CleanFileName(pk.FileName));
+            File.WriteAllBytes(fn, pk.DecryptedPartyData);
+            LogUtil.LogInfo($"Saved file: {fn}", "Dump");
+        }
+
+        public async Task<SAV8SWSH> SCVIGetFakeTrainerSAV(CancellationToken token)
+        {
+            SAV8SWSH scvi = new SAV8SWSH();
+            return scvi;
+        }
+
+
+        public async Task CleanExit(IBotStateSettings settings, CancellationToken token)
+        {
+            if (settings.ScreenOff)
+            {
+                Log("Turning on screen.");
+                await SetScreen(ScreenState.On, token).ConfigureAwait(false);
+            }
+            Log("Detaching controllers on routine exit.");
+            await DetachController(token).ConfigureAwait(false);
+        }
+        public async Task InitializeHardware(IBotStateSettings settings, CancellationToken token)
+        {
+            Log("Detaching on startup.");
+            await DetachController(token).ConfigureAwait(false);
+            if (settings.ScreenOff)
+            {
+                Log("Turning off screen.");
+                await SetScreen(ScreenState.Off, token).ConfigureAwait(false);
+            }
+        }
+    }
+}
