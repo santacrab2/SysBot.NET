19320e2337d690467f4acb78ccd77c858c578fe9
Santacrab2 <skaradiodj@gmail.com>
2022-11-22 11:29:59 -0500

    fixin typos,  experimenting

--- SysBot.Pokemon/SV/Vision/PokeDataOffsetsSCVI.cs
+++ SysBot.Pokemon/SV/Vision/PokeDataOffsetsSCVI.cs
@@ -12,7 +12,7 @@ namespace SysBot.Pokemon
         public const string ScarletID = "0100A3D008C5C000 ";
         public const string VioletID = "01008F6008C5E000";
 
-        public IReadOnlyList<long> BoxStartPokemonPointer { get; } = new long[] { 0x42DBC98, 0xb0, 0x0, 0x0, 0x30, 0xb8, 0x50, 0x10, 0x988 };
+        public static IReadOnlyList<long> BoxStartPokemonPointer { get; } = new long[] { 0x42DBC98, 0xb0, 0x0, 0x0, 0x30, 0xb8, 0x50, 0x10, 0x988 };
 
         public const int EncryptedSize = 0x158;
     }
