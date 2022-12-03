c128aadb2f68be73bfb762012c0eb73a320f186c
Santacrab2 <skaradiodj@gmail.com>
2022-12-03 10:45:23 -0500

    updating stuff, messing around with getting trainer info out of the cache for SV etc

--- SysBot.Pokemon/SV/Vision/PokeDataOffsetsSCVI.cs
+++ SysBot.Pokemon/SV/Vision/PokeDataOffsetsSCVI.cs
@@ -9,11 +9,13 @@ namespace SysBot.Pokemon
     {
         //Scarlet and Violet
 
-        public const string ScarletID = "0100A3D008C5C000 ";
+        public const string ScarletID = "0100A3D008C5C000";
         public const string VioletID = "01008F6008C5E000";
 
-        public static IReadOnlyList<long> BoxStartPokemonPointer { get; } = new long[] { 0x42DBC98, 0xb0, 0x0, 0x0, 0x30, 0xb8, 0x50, 0x10, 0x988 };
+        public static IReadOnlyList<long> BoxStartPokemonPointer { get; } = new long[] { 0x42FD510,0xA90,0x9B0 };
 
-        public const int EncryptedSize = 0x158;
+        public static IReadOnlyList<long> MyStatusBlockPointer { get; } = new long[] { 0x42F2F80, 0xA0, 0x10, 0xB0 };
+
+    public const int EncryptedSize = 0x158;
     }
 }
